2e1535173ef4469fdce9e0a886f05b8da9c56369
Vivian Munguía <andy_munguia@hotmail.com>
2020-06-28 15:20:33 -0700

    Se creó un constructor en la clase Curso.cs donde se genera el Id automáticamente

--- Entidades/Curso.cs
+++ Entidades/Curso.cs
@@ -1,9 +1,17 @@
+using System;
+
 namespace CoreEscuela.Entidades
 {
     public class Curso
     {
-        public string UniqueId { get; set; }
+        public string UniqueId { get; private set; }
         public string Nombre { get; set; }
         public TiposJornada Jornada{ get; set; }
+    
+        public Curso()
+        {
+            UniqueId = Guid.NewGuid().ToString();
+        }
+
     }
 }
