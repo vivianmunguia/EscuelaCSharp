5cc55be6ba3c834f820e11ec69baf2d29eb966a6
Vivian Munguía <andy_munguia@hotmail.com>
2020-07-11 19:01:28 -0700

    Se ordenó el código creando la clase EscuelaEngine.cs y pasando todo lo que había en la clase main

--- Program.cs
+++ Program.cs
@@ -3,49 +3,19 @@ using System.Collections.Generic;
 using CoreEscuela.Entidades;
 using static System.Console;
 
-namespace Etapa1
+namespace CoreEscuela
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria,
-            ciudad: "Bogotá", pais: "Colombia");
-
-            escuela.Cursos = new List<Curso>(){
-                new Curso(){ Nombre = "101", Jornada = TiposJornada.Mañana },
-                new Curso(){ Nombre = "201", Jornada = TiposJornada.Mañana },
-                new Curso(){ Nombre = "301", Jornada = TiposJornada.Mañana }
-            };
-
-            escuela.Cursos.Add(new Curso { Nombre = "102", Jornada = TiposJornada.Tarde });
-            escuela.Cursos.Add(new Curso { Nombre = "202", Jornada = TiposJornada.Tarde });
-
-            var otraColeccion = new List<Curso>(){
-                new Curso(){ Nombre = "401", Jornada = TiposJornada.Mañana },
-                new Curso(){ Nombre = "501", Jornada = TiposJornada.Mañana },
-                new Curso(){ Nombre = "501", Jornada = TiposJornada.Tarde }
-            };
-
-            escuela.Cursos.AddRange(otraColeccion); //Se adiciona la segunda colección a la primera
-
-            escuela.Cursos.RemoveAll(delegate (Curso cur)
-            { //Busca el curso que tenga de nombre 301 y lo remueve
-                return cur.Nombre == "301";
-            });
-            //Recibe un objeto cur, busca uno cuyo nombre sea 301 y lo borra
-            escuela.Cursos.RemoveAll((cur) => cur.Nombre == "501" && cur.Jornada == TiposJornada.Mañana);
-
+            var engine = new EscuelaEngine();
+            engine.Inicializar();
 
             WriteLine("====================");
-            ImprimirCursosEscuela(escuela);
+            ImprimirCursosEscuela(engine.Escuela);
 
         }
-
-        /*private static bool Predicado(Curso curobj)
-        {
-            return curobj.Nombre == "301";
-        }*/
 
         private static void ImprimirCursosEscuela(Escuela escuela)
         {
@@ -66,43 +36,5 @@ namespace Etapa1
 
         }
 
-        private static void ImprimirCursosWhile(Curso[] arregloCursos)
-        {
-            int contador = 0;
-            while (contador < arregloCursos.Length)
-            {
-                WriteLine($"Nombre {arregloCursos[contador].Nombre}, "
-                + $"Id {arregloCursos[contador].UniqueId}");
-                contador++;
-            }
-        }
-
-        private static void ImprimirCursosDoWhile(Curso[] arregloCursos)
-        {
-            int contador = 0;
-            do
-            {
-                WriteLine($"Nombre {arregloCursos[contador].Nombre}, "
-                + $"Id {arregloCursos[contador].UniqueId}");
-                contador++;
-            } while (contador < arregloCursos.Length);
-        }
-
-        private static void ImprimirCursosFor(Curso[] arregloCursos)
-        {
-            for (int i = 0; i < arregloCursos.Length; i++)
-            {
-                WriteLine($"Nombre {arregloCursos[i].Nombre}, "
-                + $"Id {arregloCursos[i].UniqueId}");
-            }
-        }
-
-        private static void ImprimirCursosForEach(Curso[] arregloCursos)
-        {   //para cada curso en arregloCursos
-            foreach (var curso in arregloCursos)
-            {
-                WriteLine($"Nombre {curso.Nombre}, Id {curso.UniqueId}");
-            }
-        }
     }
 }
